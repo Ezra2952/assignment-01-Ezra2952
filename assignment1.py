282df2d43cbbf9b6ee886db9dd5ddf96de775cad
Kyaw Soe Naing <kyawsoenaing@my.jcu.edu.au>
2019-12-08 19:46:44 +0800

    display menu and ask for user input

--- assignment1.py
+++ assignment1.py
@@ -2,13 +2,28 @@
 Replace the contents of this module docstring with your own details
 Name: Kyaw Soe Naing
 Date started: 29/11/2019
-GitHub URL: https://github.com/Ezra2952
+GitHub URL: https://github.com/Ezra2952/assignment-01-Ezra2952
+"""
+
+print("Movies To Watch 1.0 - by Kyaw Soe Naing")
+MENU = """Menu:
+L - List movies
+A - Add new movies
+W - Watch a movie
+Q - Quit
 """
 
 
+# Main Function including other many functions
 def main():
-    """..."""
-    print("Movies To Watch 1.0 - by <Your Name>")
+    pass
+
+
+# ask for the user to input and print menu
+def menu():
+    print(MENU)
+    user_input = input(">>>")
+    return user_input
 
 
 if __name__ == '__main__':
